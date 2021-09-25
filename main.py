8c22a67afc4b30a628da650d9ebc84fa271e03b5
Kimi <zheweiwan@gmail.com>
2021-09-24 21:15:35 -0700

    Committed and Pushed

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 from player import Player
+import random
+import time
 
 player = Player()
 
 
-def main():
-    ...
+with open('map.csv', 'r') as file:
+    board = file.readline().split(', ')
+print(board)
+
+
+def main(round_num: int, players: int) -> None:
+    round_num, players = int(round_num), int(players)
+    if players <= 1:
+        print('You have to have more than 1 player!')
+        return
+    for rounds in range(round_num):
+        time.sleep(0.5)
+        print(f'\nRound {rounds + 1}')
+        for players in range(players):
+            if player.bankrupted():
+                break
+            print(f"Player {players+1}'s turn")
+            dice_roll = random.randint(0, 6)
+            print(f"\nPlayer {players+1}'s rolled {dice_roll}")
+            time.sleep(0.5)
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        times = input('Number of rounds: ')
+        num_player = input('Number of players: ')
+        if not times:
+            times = 15
+        if not num_player:
+            num_player = 4
+        main(times, num_player)
+        if input('Do you want to go again (Yes or No): ').lower() == 'no':
+            break
+    print('Thanks for playing!')
--- player.py
+++ player.py
@@ -1,16 +1,26 @@
-import random as r
-
-
 class Player:
-    def __init(self):
-        self.starting_money = 1500
+    def __init__(self):
+        self.money = 1500
         self.bankrupt = False
-        self.triple_double = False
 
-    def dice_roll(self):
-        roll1 = r.randint(0, 6)
-        roll2 = r.randint(0, 6)
-        if roll1 == roll2:
-            return "Double!"
+    def bankrupted(self):
+        if (self.money <= 10 and self.bankrupt is False) or self.bankrupt is True:
+            self.bankrupt = True
+            return True
+        return False
+
+    def double_roll(self, current, previous):
+        if previous == current:
+            return True
+        return False
+
+    def test(self, subtracted):
+        self.money -= subtracted
+        return
 
 
+if __name__ == '__main__':
+    player = Player()
+    print(player.bankrupted())
+    player.test(1990)
+    print(player.bankrupted())
