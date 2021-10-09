7f5bbec8dc6d523b467296279e588bd33a67ea24
Kimi <zheweiwan@gmail.com>
2021-10-09 16:12:26 -0700

    Committed and Pushed

--- main.py
+++ main.py
@@ -5,37 +5,41 @@ import time
 player = Player()
 
 
-with open('map.csv', 'r') as file:
-    board = file.readline().split(', ')
-print(board)
+mapped = {}
+with open('map.json', 'r') as file:
+    for i in range(39):
+        mapped[i] = file.readline().split('\n')
+print(mapped)
+
+player_info = {}
 
 
-def main(round_num: int, players: int) -> None:
-    round_num, players = int(round_num), int(players)
+def main(info: dict, round_num=15, players=4) -> None:
     if players <= 1:
         print('You have to have more than 1 player!')
         return
     for rounds in range(round_num):
         time.sleep(0.5)
-        print(f'\nRound {rounds + 1}')
+        print(f'\nRound {rounds+1}')
         for players in range(players):
-            if player.bankrupted():
-                break
-            print(f"Player {players+1}'s turn")
-            dice_roll = random.randint(0, 6)
-            print(f"\nPlayer {players+1}'s rolled {dice_roll}")
             time.sleep(0.5)
+            if info[players][0] <= 10 or info[players][2]:
+                info[players][2] = True
+                print(f"- - - - -\n{players+1} is bankrupted, skipped...\n- - - - - -")
+                time.sleep(2)
+            else:
+                print(f"Player {players+1}'s turn")
+                dice_roll = random.randint(0, 6)
+                print(f"\nPlayer {players+1}'s rolled {dice_roll}")
 
 
 if __name__ == '__main__':
     while True:
         times = input('Number of rounds: ')
         num_player = input('Number of players: ')
-        if not times:
-            times = 15
-        if not num_player:
-            num_player = 4
-        main(times, num_player)
+        for i in range(int(num_player)):
+            player_info[i] = [1500, True, False]  # Money; Previous Roll; Bankrupt;
+        main(player_info, int(times), int(num_player))
         if input('Do you want to go again (Yes or No): ').lower() == 'no':
             break
     print('Thanks for playing!')
--- player.py
+++ player.py
@@ -1,13 +1,7 @@
 class Player:
-    def __init__(self):
-        self.money = 1500
+    def __init__(self, money=1500):
+        self.money = money
         self.bankrupt = False
-
-    def bankrupted(self):
-        if (self.money <= 10 and self.bankrupt is False) or self.bankrupt is True:
-            self.bankrupt = True
-            return True
-        return False
 
     def double_roll(self, current, previous):
         if previous == current:
@@ -21,6 +15,3 @@ class Player:
 
 if __name__ == '__main__':
     player = Player()
-    print(player.bankrupted())
-    player.test(1990)
-    print(player.bankrupted())
